213176fe9b29fd29ccc366809abe79b921d7bd09
MenekseYuncu <69850190+MenekseYuncu@users.noreply.github.com>
2022-09-17 23:02:28 +0300

    ArtBook Database

--- app/src/main/java/com/menekse/artbook/MainActivity.java
+++ app/src/main/java/com/menekse/artbook/MainActivity.java
@@ -2,9 +2,12 @@ package com.menekse.artbook;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -13,9 +16,13 @@ import android.view.View;
 
 import com.menekse.artbook.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    ArrayList<Art> artArrayList;
+    ArtAdapter artAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,37 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+        artArrayList = new ArrayList<>();
+
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        artAdapter = new ArtAdapter(artArrayList);
+        binding.recyclerView.setAdapter(artAdapter);
+        getData();
+
+    }
+
+    private void getData(){
+        try {
+            SQLiteDatabase sqLiteDatabase = this.openOrCreateDatabase("Arts",MODE_PRIVATE,null);
+
+            Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM arts",null);
+            int nameIx = cursor.getColumnIndex("artname");
+            int idIx = cursor.getColumnIndex("id");
+
+            while (cursor.moveToNext()){
+                String name = cursor.getString(nameIx);
+                int id= cursor.getInt(idIx);
+                Art art = new Art(name,id);
+                artArrayList.add(art);
+            }
+            //yeni veri geldiğinde recyclerView e haber veriyor mutlaka yazılması gerekir verinin güncellediğini görmek için
+            artAdapter.notifyDataSetChanged();
+          cursor.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 
